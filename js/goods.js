e96369860c7fe91390f6b942f47bf94fd0aea009
lifedok <lifedok@gmail.com>
2018-09-18 01:01:58 +0300

    Add items for basket

--- js/goods.js
+++ js/goods.js
@@ -101,22 +101,21 @@ var getSrcUrl = function (item) {
 
 // функция для нахождения рандомного булевого значения
 var getRandomBoolean = function () {
-  var cons = 10;
-  var value = Math.floor(Math.random() * cons);
-  return value < cons / 2;
+  return Math.random() <= 0.5;
 };
 
 var COUNT_VIEW_ITEM = 26;
-// var COUNT_BASCET_ITEM = 3;
+var COUNT_BASKET_ITEM = 3;
+var goods = [];
+var basketGoods = [];
 
 // функция для создания массива и 26 сгенерированных объектов
 var randomItem = function () {
-  var arr = [];
   for (var i = 0; i < COUNT_VIEW_ITEM; i++) {
-    arr.push({
+    goods.push({
       name: getRandomElement(NAME),
       picture: getSrcUrl(getRandomElement(IMAGE)),
-      amount: getRandomValue(0, 20),
+      amounts: getRandomValue(0, 20),
       price: getRandomValue(100, 1500),
       weight: getRandomValue(30, 300),
       rating: {
@@ -130,114 +129,116 @@ var randomItem = function () {
       }
     });
   }
-  return arr;
 };
+randomItem();
 
 
-// var CARD_BASKET_TEMPLATE = document.querySelector('#cards-empty').content;
 var CARD_TEMPLATE = document.querySelector('#card').content;
 var CATALOG_CARDS = document.querySelector('.catalog__cards');
 var CATALOG_LOAD = document.querySelector('.catalog__load');
-
-var GOODS_CARDS = document.querySelector('.goods__cards');
-var GOODS_CARD_EMPTY = document.querySelector('.goods__card-empty');
-
 
 // удаляем класс catalog__cards--load и добавляем класс catalog__load
 CATALOG_CARDS.classList.remove('catalog__cards--load');
 CATALOG_LOAD.classList.add('visually-hidden');
 
-// удаляем класс goods__cards--empty и
-// скрываем блок goods__card-empty классом visually-hidden
-GOODS_CARDS.classList.remove('goods__cards--empty');
-GOODS_CARD_EMPTY.classList.add('visually-hidden');
-
-
 // на основе данных и шаблона catalog__card создаём DOM-элемент с товарами
 var renderItem = function (arr) {
+  var amount = arr.amounts;
+  var name = arr.name;
+  var price = arr.price;
+  var picture = arr.picture;
+  var weight = arr.weight;
+  var ratingValue = arr.rating.value;
+  var ratingNumber = arr.rating.number;
+  var nutritionFactsSugar = arr.nutritionFacts.sugar;
+  var nutritionFactsContents = arr.nutritionFacts.contents;
+
   var catalogCard = CARD_TEMPLATE.querySelector('.catalog__card').cloneNode(true);
-  var cardPrice = catalogCard.querySelector('.card__price');
   var starsRating = catalogCard.querySelector('.stars__rating');
-  var starCount = catalogCard.querySelector('.star__count');
-  var cardCharacteristic = catalogCard.querySelector('.card__characteristic');
-  var cardCompositionList = catalogCard.querySelector('.card__composition-list');
+  catalogCard.querySelector('.card__img').src = picture;
 
-  for (var i = 0; i < arr.length; i++) {
-    var amount = arr[i].amount;
-    var name = arr[i].name;
-    var price = arr[i].price;
-    var weight = arr[i].weight;
-    var ratingValue = arr[i].rating.value;
-    var ratingNumber = arr[i].rating.number;
-    var nutritionFactsSugar = arr[i].nutritionFacts.sugar;
-    var nutritionFactsContents = arr[i].nutritionFacts.contents;
+  catalogCard.querySelector('.card__weight').textContent = weight;
+  catalogCard.querySelector('.card__price').textContent = price;
+  catalogCard.querySelector('.star__count').textContent = ratingNumber;
+  catalogCard.querySelector('.card__title').textContent = name;
+  catalogCard.querySelector('.card__characteristic').textContent = nutritionFactsSugar === true ? 'Содержит сахар' : 'Без сахара';
+  catalogCard.querySelector('.card__composition-list').textContent = nutritionFactsContents;
 
-    // добавляем дополнительный класс в зависимости от amount
-    if (amount > 5) {
-      return catalogCard.classList.add('card--in-stock');
-    } else if (amount > 1 && amount < 5) {
-      return catalogCard.classList.add('card--title');
-    } else if (amount === 0) {
-      return catalogCard.classList.add('card--soon');
-    }
+  // устанавливаем рейтинг в зависимости от rating.value
+  var starsClass = function (string) {
+    return starsRating.classList.add('stars__rating--' + string);
+  };
+  switch (ratingValue) {
+    case 1:
+      starsClass('one');
+      break;
+    case 2:
+      starsClass('two');
+      break;
+    case 3:
+      starsClass('three');
+      break;
+    case 4:
+      starsClass('four');
+      break;
+    case 5:
+      starsClass('five');
+      break;
+  }
 
-    // добавляем название, цену и вес
-    catalogCard.querySelector('.card__title').textContent = name;
-    cardPrice.textContent = price;
-    cardPrice.querySelector('.card__weight').textContent = weight;
-    starCount.textContent = ratingNumber;
-
-    // устанавливаем рейтинг в зависимости от rating.value
-    switch (ratingValue) {
-      case 1:
-        starsRating.classList.add('stars__rating--one');
-        break;
-      case 2:
-        starsRating.classList.add('stars__rating--two');
-        break;
-      case 3:
-        starsRating.classList.add('stars__rating--three');
-        break;
-      case 4:
-        starsRating.classList.add('stars__rating--four');
-        break;
-      case 5:
-        starsRating.classList.add('stars__rating--five');
-        break;
-    }
-
-    cardCharacteristic.textContent = nutritionFactsSugar === true ? 'Содержит сахар' : 'Без сахара';
-    cardCompositionList.textContent = nutritionFactsContents;
+  if (amount > 5) {
+    catalogCard.classList.add('card--in-stock');
+  } else if (amount > 1 && amount < 5) {
+    catalogCard.classList.add('card--title');
+  } else if (amount === 0) {
+    catalogCard.classList.add('card--soon');
   }
   return catalogCard;
 };
 
-// отрисовка сгенерированного DOM-элемента в блок .catalog__load
 var getItemList = function () {
-  var items = randomItem();
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < items.length; i++) {
-    fragment.appendChild(renderItem(items[i]));
+  for (var i = 0; i < goods.length; i++) {
+    fragment.appendChild(renderItem(goods[i]));
   }
   CATALOG_CARDS.appendChild(fragment);
 };
 getItemList();
 
-// отрисовка basket DOM-элемента в блок .goods__cards
-// var getItemsForBasket = function (arr, value) {
-//   arr = [];
-//   var fragment = document.createDocumentFragment();
-//   for (var i = 0; i < arr.length; i++) {
-//     fragment.appendChild(renderItem(items[i]));
-//   }
-//   CATALOG_CARDS.appendChild(fragment);
-// };
+
+var CARD_BASKET_TEMPLATE = document.querySelector('#card-order').content;
+var GOODS_CARDS = document.querySelector('.goods__cards');
+var GOODS_CARD_EMPTY = document.querySelector('.goods__card-empty');
+GOODS_CARDS.classList.remove('goods__cards--empty');
+GOODS_CARD_EMPTY.classList.add('visually-hidden');
+
+// добавление в корзину
+var getBasketItems = function () {
+  var startIndex = getRandomValue(0, goods.length - COUNT_BASKET_ITEM - 1);
+  basketGoods = goods.slice(startIndex, startIndex + COUNT_BASKET_ITEM);
+};
+getBasketItems();
+
+var renderBasketItem = function (arr) {
+  var name = arr.name;
+  var picture = arr.picture;
+  var price = arr.price;
+  var amount = arr.amounts;
+
+  var catalogBasketCard = CARD_BASKET_TEMPLATE.querySelector('.goods_card').cloneNode(true);
+  catalogBasketCard.querySelector('.card-order__title').textContent = name;
+  catalogBasketCard.querySelector('.card-order__img').src = picture;
+  catalogBasketCard.querySelector('.card-order__price').textContent = price;
+  catalogBasketCard.querySelector('.card-order__count').value = amount;
+  return catalogBasketCard;
+};
 
 
-// console.log('GOODS_CARDS', GOODS_CARDS);
-// console.log('CATALOG_CARDS', CATALOG_CARDS);
-// console.log('CATALOG_LOAD', CATALOG_LOAD);
-// console.log('CARD_ORDER_TEMPLATE', CARD_TEMPLATE);
-// console.log('catalogCard', CARD_TEMPLATE.querySelector('.catalog__card').cloneNode(true));
-
-
+var getBasketItemList = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < basketGoods.length; i++) {
+    fragment.appendChild(renderBasketItem(basketGoods[i]));
+  }
+  GOODS_CARDS.appendChild(fragment);
+};
+getBasketItemList();
